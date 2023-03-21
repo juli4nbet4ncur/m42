9ef7dc0ab225cf87aeb4959c83b3e40bc8fd502e
Julian Betancur <juli4nbet4ncur@gmail.com>
2023-03-21 10:54:06 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ const templateContent = ({ htmlWebpackPlugin }) => `
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="description" content="Progressive Web Application with 12 open source frontend focused tools">
+      <meta name="description" content="Tiny tools for easy learning.">
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap" rel="stylesheet">
